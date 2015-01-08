d8aecaf71c5e92b9e67b5a44e510d90a2bd56bd0
Kazark <kazark@zoho.com>
2015-01-08 13:06:17 -0500

    Added an empty (portable, separate) Eto solution
    https://github.com/picoe/Eto/wiki/Quick-Start

---
+++ Void/Void.Gtk3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eto.Forms;
+
+namespace Void.Gtk3
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Gtk3).Run(new MainForm());
+        }
+    }
+}
+
---
+++ Void/Void.WinForms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eto.Forms;
+
+namespace Void.WinForms
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.WinForms).Run(new MainForm());
+        }
+    }
+}
+
