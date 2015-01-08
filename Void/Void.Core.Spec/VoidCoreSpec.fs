19633b9ab83584611b98bc7abfd9e62533656eb5
Kazark <kazark@zoho.com>
2015-01-08 13:36:34 -0500

    Added some F# libraries for the core logic of Void

---
+++ Void/Void.Core.Spec/VoidCoreSpec.fs
@@ -0,0 +1,10 @@
+﻿namespace Void.Core.Spec
+
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type ``Void core``() = 
+    [<Test>]
+     member x.``should be bootstrapped properly``() =
+        Void.Core.Editor.bootstrapped |> should be True
