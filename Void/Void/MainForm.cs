3df3b87be33ec547e0d95ab211227f35491a483a
Kazark <kazark@zoho.com>
2015-01-10 16:13:27 -0500

    Fiddled around with the main form a bit

--- Void/Void/MainForm.cs
+++ Void/Void/MainForm.cs
@@ -7,30 +7,22 @@ namespace Void
     {
         public MainForm()
         {
-            Title = "My Eto Form";
-            ClientSize = new Size(400, 350);
+            Title = "Void - A text editor in the spirit of Vim";
+            ClientSize = new Size(480, 320);
 
             // scrollable region as the main content
             Content = new Scrollable
             {
-                // table with three rows
                 Content = new TableLayout(
-                    null,
-                    // row with three columns
-                    new TableRow(null, new Label { Text = "Hello World!" }, null),
-                    null
-                )
+                    new TableRow(new Label
+                    {
+                        Text = "Hello World!",
+                        TextColor = Color.FromArgb(255, 255, 255),
+                        Font = Fonts.Monospace(9)
+                    })
+                ),
+                BackgroundColor = Color.FromArgb(0, 0, 0)
             };
-
-            // create a few commands that can be used for the menu and toolbar
-            var clickMe = new Command { MenuText = "Click Me!", ToolBarText = "Click Me!" };
-            clickMe.Executed += (sender, e) => MessageBox.Show(this, "I was clicked!");
-
-            var quitCommand = new Command { MenuText = "Quit", Shortcut = Application.Instance.CommonModifier | Keys.Q };
-            quitCommand.Executed += (sender, e) => Application.Instance.Quit();
-
-            var aboutCommand = new Command { MenuText = "About..." };
-            aboutCommand.Executed += (sender, e) => MessageBox.Show(this, "About my app...");
 
             Menu = new MenuBar();
         }
