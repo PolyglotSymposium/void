bc564dbc3dc989e76d0973d2eed0be6233d975ca
Kazark <kazark@zoho.com>
2015-01-10 20:27:39 -0500

    Size horizontally...will this work on Windows?

--- Void/Void/MainForm.cs
+++ Void/Void/MainForm.cs
@@ -14,10 +14,11 @@ namespace Void
         {
             var verticalPadding = Platform.IsGtk ? 0 : 3;
             _lineHeight = Convert.ToInt32(Math.Ceiling(_font.LineHeight))+verticalPadding;
+            var lineWidth = Convert.ToInt32(Math.Ceiling(_font.XHeight));
             Title = "Void - A text editor in the spirit of Vim";
             const int NumberOfRows = 25;
             var height = _lineHeight * NumberOfRows + verticalPadding;
-			var size = new Size(480, height);
+			var size = new Size(lineWidth*80, height);
 			BackgroundColor = Color.FromArgb(0, 0, 0);
 			Content = PopulateContent(size, NumberOfRows);
 			ClientSize = size;
@@ -26,22 +27,31 @@ namespace Void
 		private Control PopulateContent(Size size, int numberOfRows)
 		{
 			var content = new TableLayout(size);
-			foreach (var i in Enumerable.Range(0, numberOfRows))
+            var offset0 = Convert.ToInt32(Math.Floor(_font.LineHeight))*0;
+            content.Add(MakeLabel("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"), new Point(0, offset0));
+			foreach (var i in Enumerable.Range(1, numberOfRows-2))
 			{
 			    var offset = Convert.ToInt32(Math.Floor(_font.LineHeight))*i;
 				content.Add(MakeLabel(i), new Point(0, offset));
 			}
+            var offset25 = Convert.ToInt32(Math.Floor(_font.LineHeight))*numberOfRows-1;
+            content.Add(MakeLabel("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"), new Point(0, offset25));
 			return content;
 		}
 
+        private Label MakeLabel(string text)
+        {
+            return new Label {
+                Text = text,
+                TextColor = Color.FromArgb(255, 255, 255),
+                Font = _font,
+                Height = _lineHeight
+            };
+        }
+
 		private Label MakeLabel(int counter)
 		{
-			return new Label {
-				Text = "Line #" + counter,
-				TextColor = Color.FromArgb(255, 255, 255),
-				Font = _font,
-                Height = _lineHeight
-			};
+            return MakeLabel("Line #" + counter);
 		}
     }
 }
