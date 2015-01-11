2df9a88c83e82d419af0321aa0133d3d1924d012
Kazark <kazark@zoho.com>
2015-01-10 20:02:15 -0500

    Hacked line height & placement to work on Windows
    The sizing was not working right on Windows with the cleaner solution.
    I'm going to pull this down on my Linux box and see if this hacked
    version works there. Something inside me is wondering if there is a bug
    in ETO. I am probably going to have problems with horizontal sizing,
    too, once I get that in place. Right now I'm focused on getting this
    project off the ground so I am not too concerned about the hack.

--- Void/Void/MainForm.cs
+++ Void/Void/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Eto.Forms;
 using Eto.Drawing;
 
@@ -6,34 +7,40 @@ namespace Void
 {
     public class MainForm : Form
     {
-		private Font _font = Fonts.Monospace(9);
+		private readonly Font _font = Fonts.Monospace(9);
+        private readonly int _lineHeight;
 
         public MainForm()
         {
+            const int LinePadding = 3;
+            _lineHeight = Convert.ToInt32(Math.Ceiling(_font.LineHeight))+LinePadding;
             Title = "Void - A text editor in the spirit of Vim";
-			var size = new Size(480, System.Convert.ToInt32(System.Math.Ceiling(_font.LineHeight*25)));
-			ClientSize = size;
+            const int NumberOfRows = 25;
+            var height = _lineHeight * NumberOfRows + LinePadding;
+			var size = new Size(480, height);
 			BackgroundColor = Color.FromArgb(0, 0, 0);
-			Content = PopulateContent(size);
+			Content = PopulateContent(size, NumberOfRows);
+			ClientSize = size;
         }
 
-		private Control PopulateContent(Size size)
+		private Control PopulateContent(Size size, int numberOfRows)
 		{
 			var content = new TableLayout(size);
-			foreach (var i in Enumerable.Range(0, 25))
+			foreach (var i in Enumerable.Range(0, numberOfRows))
 			{
-				content.Add(MakeLabel(i), new Point(0, 17*i));
+			    var offset = Convert.ToInt32(Math.Floor(_font.LineHeight))*i;
+				content.Add(MakeLabel(i), new Point(0, offset));
 			}
 			return content;
 		}
-
 
 		private Label MakeLabel(int counter)
 		{
 			return new Label {
 				Text = "Line #" + counter,
 				TextColor = Color.FromArgb(255, 255, 255),
-				Font = _font
+				Font = _font,
+                Height = _lineHeight
 			};
 		}
     }
