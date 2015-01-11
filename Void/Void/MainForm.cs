e70b4a549290adaa19bacaa28fb12b39738bf2b2
Kazark <kazark@zoho.com>
2015-01-10 19:09:26 -0500

    Further playing with GUI, ramping up to real work
    Enjoying ETO pretty well and working in MonoDevelop. Enjoying that too
    fairly well, though some aspects of it are underwhelming. The lightness
    of it (as compared to Visual Studio), though, is much appreciated.

--- Void/Void/MainForm.cs
+++ Void/Void/MainForm.cs
@@ -1,30 +1,40 @@
-﻿using Eto.Forms;
+﻿using System.Linq;
+using Eto.Forms;
 using Eto.Drawing;
 
 namespace Void
 {
     public class MainForm : Form
     {
+		private Font _font = Fonts.Monospace(9);
+
         public MainForm()
         {
             Title = "Void - A text editor in the spirit of Vim";
-            ClientSize = new Size(480, 320);
-
-            // scrollable region as the main content
-            Content = new Scrollable
-            {
-                Content = new TableLayout(
-                    new TableRow(new Label
-                    {
-                        Text = "Hello World!",
-                        TextColor = Color.FromArgb(255, 255, 255),
-                        Font = Fonts.Monospace(9)
-                    })
-                ),
-                BackgroundColor = Color.FromArgb(0, 0, 0)
-            };
-
-            Menu = new MenuBar();
+			var size = new Size(480, System.Convert.ToInt32(System.Math.Ceiling(_font.LineHeight*25)));
+			ClientSize = size;
+			BackgroundColor = Color.FromArgb(0, 0, 0);
+			Content = PopulateContent(size);
         }
+
+		private Control PopulateContent(Size size)
+		{
+			var content = new TableLayout(size);
+			foreach (var i in Enumerable.Range(0, 25))
+			{
+				content.Add(MakeLabel(i), new Point(0, 17*i));
+			}
+			return content;
+		}
+
+
+		private Label MakeLabel(int counter)
+		{
+			return new Label {
+				Text = "Line #" + counter,
+				TextColor = Color.FromArgb(255, 255, 255),
+				Font = _font
+			};
+		}
     }
 }
