76d8dc4fdd2fc5bb31b6194c9f9e3472b3e570ff
Kazark <kazark@zoho.com>
2015-03-21 11:29:17 -0400

    Added project for Void#Editor VoidScript module
    This will contain all your standard, basic VoidScript commands which are
    specific to the editor rather than the language, i.e. ;subsitute, ;edit,
    ;redraw, etc.

---
+++ src/Void.Lang.Editor/VoidScriptEditorModule.fs
@@ -0,0 +1,21 @@
+﻿namespace Void.Lang.Editor
+
+open Void.Lang.Parser
+open Void.Lang.Interpreter
+
+module VoidScriptEditorModule =
+    let commands = [
+        {
+            Definition =
+                {
+                    ShortName = ""
+                    FullName = ""
+                    AcceptsRange = false
+                    Type = CommandType.Nullary
+                }
+            Execute = fun _ -> ()
+        }
+    ]
+
+type Class1() = 
+    member this.X = "F#"
--- src/Void.Lang.Interpreter/Interpreter.fs
+++ src/Void.Lang.Interpreter/Interpreter.fs
@@ -1,4 +1,11 @@
 ﻿namespace Void.Lang.Interpreter
 
+open Void.Lang.Parser
+
+type VoidScriptCommand = {
+    Definition : CommandDefinition
+    Execute : CommandArguments -> unit
+}
+
 module Interpreter =
     let bootstrapped = true
