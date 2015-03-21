9e8004c3d48a09383c432d9bac6e7166a32bb389
Kazark <kazark@zoho.com>
2015-03-21 11:10:38 -0400

    Extracted a top-level module to glue things together

--- Void/Void.ViewModel/ViewController.fs
+++ Void/Void.ViewModel/ViewController.fs
@@ -14,8 +14,6 @@ type ViewController
     let _colorscheme = Colors.defaultColorscheme
     let mutable _bufferedDrawings = Seq.empty
 
-    // Subscribe to some init event on the view instead of exposing this as a
-    // public method?
     member private x.initializeView() =
         _view.SetViewTitle ViewModel.defaultTitle
         _view.SetBackgroundColor Colors.defaultColorscheme.Background
@@ -77,52 +75,3 @@ type ViewController
             _convert.cellBlockToPixels (ViewModel.wholeArea _viewModel) |> _view.TriggerDraw // TODO shouldn't redraw the whole UI
         | _ -> ()
         Command.Noop
-
-type Broker
-    (
-        _commandHandlers : (Command -> Command) list,
-        _eventHandlers : (Event -> Command) list,
-        _viewCtrl : ViewController,
-        _normalCtrl : NormalModeController
-    ) =
-
-    member x.brokerViewEvent viewEvent =
-        match viewEvent with
-        | ViewEvent.PaintInitiated draw ->
-            _viewCtrl.paint draw
-        | ViewEvent.KeyPressed keyPress ->
-            _normalCtrl.handleKeyPress keyPress |> x.brokerCommand
-        | ViewEvent.TextEntered text ->
-            () // TODO implement input and command modes, etc
-
-    member x.brokerCommand command =
-        match command with
-        | Command.PublishEvent event ->
-            for handle in _eventHandlers do
-                handle event |> x.brokerCommand
-        | Command.Noop -> ()
-        | _ ->
-            for handle in _commandHandlers do
-                handle command |> x.brokerCommand
-
-module Init =
-    let initializeVoid view =
-        let modeCtrl = ModeController()
-        let messageCtrl = MessageController()
-        let normalCtrl = NormalModeController()
-        let editorCtrl = EditorController()
-        let viewCtrl = ViewController view
-        let commandHandlers = [
-            messageCtrl.handleCommand
-            modeCtrl.handleCommand
-            viewCtrl.handleCommand
-            editorCtrl.handleCommand
-        ]
-        let eventHandlers = [
-            messageCtrl.handleEvent
-            viewCtrl.handleEvent
-        ]
-        let broker = Broker(commandHandlers, eventHandlers, viewCtrl, normalCtrl)
-        broker.brokerCommand Command.InitializeVoid
-        //broker.brokerCommand Command.ViewTestBuffer // TODO for testing and debugging only
-        broker.brokerViewEvent
---
+++ Void/Void/Init.fs
@@ -0,0 +1,53 @@
+﻿namespace Void
+
+open Void.Core
+open Void.ViewModel
+
+type Broker
+    (
+        _commandHandlers : (Command -> Command) list,
+        _eventHandlers : (Event -> Command) list,
+        _viewCtrl : ViewController,
+        _normalCtrl : NormalModeController
+    ) =
+
+    member x.brokerViewEvent viewEvent =
+        match viewEvent with
+        | ViewEvent.PaintInitiated draw ->
+            _viewCtrl.paint draw
+        | ViewEvent.KeyPressed keyPress ->
+            _normalCtrl.handleKeyPress keyPress |> x.brokerCommand
+        | ViewEvent.TextEntered text ->
+            () // TODO implement input and command modes, etc
+
+    member x.brokerCommand command =
+        match command with
+        | Command.PublishEvent event ->
+            for handle in _eventHandlers do
+                handle event |> x.brokerCommand
+        | Command.Noop -> ()
+        | _ ->
+            for handle in _commandHandlers do
+                handle command |> x.brokerCommand
+
+module Init =
+    let initializeVoid view =
+        let modeCtrl = ModeController()
+        let messageCtrl = MessageController()
+        let normalCtrl = NormalModeController()
+        let editorCtrl = EditorController()
+        let viewCtrl = ViewController view
+        let commandHandlers = [
+            messageCtrl.handleCommand
+            modeCtrl.handleCommand
+            viewCtrl.handleCommand
+            editorCtrl.handleCommand
+        ]
+        let eventHandlers = [
+            messageCtrl.handleEvent
+            viewCtrl.handleEvent
+        ]
+        let broker = Broker(commandHandlers, eventHandlers, viewCtrl, normalCtrl)
+        broker.brokerCommand Command.InitializeVoid
+        //broker.brokerCommand Command.ViewTestBuffer // TODO for testing and debugging only
+        broker.brokerViewEvent
