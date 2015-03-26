1f466c1db4b50216268c2fa2a368f3189c297b61
Kazark <kazark@zoho.com>
2015-03-26 12:20:35 -0400

    Sketched out a few builtin commands

---
+++ src/Void.Lang.Interpreter/BuiltinCommands.fs
@@ -0,0 +1,28 @@
+﻿namespace Void.Lang.Interpreter
+
+open Void.Lang.Parser
+
+module BuiltinCommands =
+    let call expressions execEnv =
+        () // TODO
+    let beginIf expressions execEnv =
+        () // TODO
+    let endIf _ execEnv =
+        () // TODO
+    let commands = [
+        {
+            ShortName = "cal"
+            FullName = "call"
+            WrapArguments = CommandType.Expression call
+        }
+        {
+            ShortName = "if"
+            FullName = "if"
+            WrapArguments = CommandType.Expression beginIf
+        }
+        {
+            ShortName = "en"
+            FullName = "endif"
+            WrapArguments = CommandType.Nullary endIf
+        }
+    ]
