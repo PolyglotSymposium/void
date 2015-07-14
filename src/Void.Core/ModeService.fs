4f6070da0919b162d0fa962a39528d586260c8e7
Kazark <kazark@zoho.com>
2015-07-13 23:13:50 -0400

    Clean up from refactor
    Manually regression tested

--- src/Void.Core/ModeService.fs
+++ src/Void.Core/ModeService.fs
@@ -1,7 +1,5 @@
 ﻿namespace Void.Core
 
-open NormalMode
-
 [<RequireQualifiedAccess>]
 type InputMode<'Output> =
     | KeyPresses of (KeyPress -> 'Output)
@@ -20,16 +18,16 @@ type CommandModeInputHandler(interpret : RequestAPI.InterpretScriptFragment) =
         Service.wrap _buffer CommandMode.handleHistoryEvent
 
 type NormalModeInputHandler() =
-    let _bindings = ref defaultBindings
-    let mutable _state = noKeysYet
+    let _bindings = ref NormalMode.emptyBindings
+    let mutable _state = NormalMode.noKeysYet
 
     member x.handleKeyPress keyPress =
-        match parse !_bindings keyPress _state with
-        | ParseResult.AwaitingKeyPress prevKeys ->
+        match NormalMode.parse !_bindings keyPress _state with
+        | NormalMode.ParseResult.AwaitingKeyPress prevKeys ->
             _state <- prevKeys
             noMessage
-        | ParseResult.CommandMatched command ->
-            _state <- noKeysYet
+        | NormalMode.ParseResult.CommandMatched command ->
+            _state <- NormalMode.noKeysYet
             command :> Message
 
     member x.handleCommand =
--- src/Void.Core/NormalMode.fs
+++ src/Void.Core/NormalMode.fs
@@ -4,6 +4,8 @@ open System.Collections.Generic
 type NormalBindings = Map<KeyPress list, CommandMessage>
 
 module NormalMode =
+    let emptyBindings =
+        Map.empty<KeyPress list, CommandMessage>
 
     [<RequireQualifiedAccess>]
     type Command =
@@ -19,18 +21,6 @@ module NormalMode =
     type ParseResult =
         | AwaitingKeyPress of KeyPress list
         | CommandMatched of CommandMessage
-
-    let defaultBindings = // TODO split out into "Vim default bindings" and "Void default bindings"
-        [
-            [KeyPress.Semicolon], CoreCommand.ChangeToMode Mode.Command :> CommandMessage
-            [KeyPress.ControlC], CoreCommand.Yank :> CommandMessage
-            [KeyPress.ControlV], CoreCommand.Put :> CommandMessage
-            [KeyPress.ControlB], CoreCommand.ChangeToMode Mode.VisualBlock :> CommandMessage
-            [KeyPress.ControlL], CoreCommand.Redraw :> CommandMessage
-            [KeyPress.ShiftZ; KeyPress.ShiftQ], CoreCommand.QuitWithoutSaving :> CommandMessage
-            [KeyPress.ShiftZ; KeyPress.ShiftA], CoreCommand.QuitAll :> CommandMessage
-            [KeyPress.G; KeyPress.Q; KeyPress.Q], CoreCommand.FormatCurrentLine :> CommandMessage
-        ] |> Map.ofList
 
     let noKeysYet =
         List.empty<KeyPress>
