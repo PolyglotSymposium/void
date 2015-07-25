150546697d5074981442672933a89f0eee15c9f0
Kazark <kazark@zoho.com>
2015-07-25 17:24:18 -0400

    Implement timeout in normal mode; resolves #53

--- src/Void.Core/NormalMode.fs
+++ src/Void.Core/NormalMode.fs
@@ -3,10 +3,14 @@ open System.Collections.Generic
 
 type NormalBindings = Map<KeyPress list, CommandMessage>
 
-module NormalMode =
-    let emptyBindings =
-        Map.empty<KeyPress list, CommandMessage>
+type KeyPressed =
+    {
+        Key : KeyPress
+        Timestamp : System.DateTime
+    }
+    interface EventMessage
 
+module NormalMode =
     [<RequireQualifiedAccess>]
     type Command =
         | Bind of KeyPress list * CommandMessage
@@ -14,48 +18,81 @@ module NormalMode =
 
     [<RequireQualifiedAccess>]
     type Event =
-        | KeysBoundToCommand
+        | KeysBoundToCommand of KeyPress list * CommandMessage
+        | KeyPressRegistered of KeyPress
+        | KeyPressesCleared
+        | ExpiredKeyPressReplacedWith of KeyPress
         interface EventMessage
 
-    [<RequireQualifiedAccess>]
-    type ParseResult =
-        | AwaitingKeyPress of KeyPress list
-        | CommandMatched of CommandMessage
+    type State = {
+        Bindings : NormalBindings
+        KeyPressBuffer : KeyPress list
+        ExpireTime : System.DateTime
+    }
 
-    let noKeysYet =
-        List.empty<KeyPress>
+    let empty =
+        {
+            Bindings = Map.empty<KeyPress list, CommandMessage>
+            KeyPressBuffer = []
+            ExpireTime = System.DateTime.MaxValue
+        }
 
-    let bind (bindings : NormalBindings) keyPresses command =
-        bindings.Add(keyPresses, command)
+    let private update state keyPress keyPresses =
+        {
+            Bindings = state.Bindings
+            KeyPressBuffer = keyPresses
+            ExpireTime = keyPress.Timestamp.AddSeconds 1.0
+        }
+        
+    let private reset state =
+        {
+            Bindings = state.Bindings
+            KeyPressBuffer = []
+            ExpireTime = empty.ExpireTime
+        }
 
-    let parse (bindings : NormalBindings) keyPress prevKeys =
-        if keyPress = KeyPress.Escape then
-            ParseResult.AwaitingKeyPress noKeysYet
+    let private bind state keyPresses command =
+        { state with Bindings = state.Bindings.Add(keyPresses, command) }
+
+    let private replaceExpired state keyPress =
+        update state keyPress [keyPress.Key], Event.ExpiredKeyPressReplacedWith keyPress.Key :> Message
+
+    let private matched state keyPress keysInBindOrder =
+        reset state, state.Bindings.Item keysInBindOrder :> Message
+
+    let private awaitingFurtherKeys state keyPress keyPresses =
+        update state keyPress keyPresses, Event.KeyPressRegistered keyPress.Key :> Message
+
+    let matchExpired state keyPress =
+        let keyPresses = [keyPress.Key]
+        if state.Bindings.ContainsKey keyPresses
+        then matched state keyPress keyPresses
+        else replaceExpired state keyPress
+
+    let private matchUnexpired state keyPress =
+        let keyPresses = keyPress.Key :: state.KeyPressBuffer
+        let inBindOrder = List.rev keyPresses
+        if state.Bindings.ContainsKey inBindOrder
+        then matched state keyPress inBindOrder
+        else awaitingFurtherKeys state keyPress keyPresses
+
+    let handleKeyPress state keyPress =
+        if keyPress.Key = KeyPress.Escape then
+            reset state, Event.KeyPressesCleared :> Message
         else
-            let keyPresses = keyPress :: prevKeys
-            let inBindOrder = List.rev keyPresses
-            if bindings.ContainsKey inBindOrder then
-                bindings.Item inBindOrder |> ParseResult.CommandMatched
-            else
-                ParseResult.AwaitingKeyPress keyPresses
+            if keyPress.Timestamp < state.ExpireTime
+            then matchUnexpired state keyPress
+            else matchExpired state keyPress
 
-    let handleCommand bindings command =
-        match command with
-        | Command.Bind (keyPresses, bindToCommand) ->
-            bind bindings keyPresses bindToCommand, Event.KeysBoundToCommand :> Message
+    let handleCommand state (Command.Bind (keyPresses, bindToCommand)) =
+        bind state keyPresses bindToCommand, Event.KeysBoundToCommand (keyPresses, bindToCommand) :> Message
 
     type InputHandler() =
-        let _bindings = ref emptyBindings
-        let mutable _state = noKeysYet
+        let _state = ref empty
 
-        member x.handleKeyPress keyPress =
-            match parse !_bindings keyPress _state with
-            | ParseResult.AwaitingKeyPress prevKeys ->
-                _state <- prevKeys
-                noMessage
-            | ParseResult.CommandMatched command ->
-                _state <- noKeysYet
-                command :> Message
+        member x.handleKeyPress (keyPress : KeyPress) =
+            let keyPressed = { Key = keyPress; Timestamp = System.DateTime.Now } // TODO push this up to the view
+            in Service.wrap _state handleKeyPress keyPressed
 
         member x.handleCommand =
-            Service.wrap _bindings handleCommand
+            Service.wrap _state handleCommand
