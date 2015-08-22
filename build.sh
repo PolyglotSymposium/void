7e7426a00f57a99924f8b6e063ad9ce69644c4af
Kazark <kazark@zoho.com>
2015-08-21 22:07:08 -0400

    Use FAKE when building in Travis CI, like local
    It have found out by experience that it is wise to not minimize
    discrepancies between your local and CI builds.

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ PACKAGE_DIR=$SOURCE_DIR/packages
 NUGET_BIN=$NUGET_DIR/NuGet.exe
 FAKE_BIN=$PACKAGE_DIR/FAKE/tools/FAKE.exe
 
-git update-index --assume-unchanged $NUGET_BIN
 chmod +x $NUGET_BIN
 [ -f "$FAKE_BIN" ] || $NUGET_BIN Install FAKE -OutputDirectory $PACKAGE_DIR -ExcludeVersion
 chmod +x $FAKE_BIN
 $FAKE_BIN build.fsx
+git checkout $NUGET_BIN
