3be2bd4412a9818dc90ab43c84379538f12f4cb2
Kazark <kazark@zoho.com>
2015-08-21 21:36:50 -0400

    Build Void with FAKE!! The way it should be.
    Need to add a build.bat or build.ps1 for Windows at some point.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ROOT_DIR=$PWD
+SOURCE_DIR=$PWD/src
+NUGET_DIR=$SOURCE_DIR/.nuget
+PACKAGE_DIR=$SOURCE_DIR/packages
+NUGET_BIN=$NUGET_DIR/NuGet.exe
+FAKE_BIN=$PACKAGE_DIR/FAKE/tools/FAKE.exe
+
+git update-index --assume-unchanged $NUGET_BIN
+chmod +x $NUGET_BIN
+[ -f "$FAKE_BIN" ] || $NUGET_BIN Install FAKE -OutputDirectory $PACKAGE_DIR -ExcludeVersion
+chmod +x $FAKE_BIN
+$FAKE_BIN build.fsx
