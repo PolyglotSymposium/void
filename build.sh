46ad43353395950c0e5a3f0f41dae54964c43b91
Kazark <kazark@zoho.com>
2015-08-21 22:22:51 -0400

    Make Travis build work correctly with build.sh
    And make it break the build if the FAKE build fails

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ NUGET_BIN=$NUGET_DIR/NuGet.exe
 FAKE_BIN=$PACKAGE_DIR/FAKE/tools/FAKE.exe
 
 chmod +x $NUGET_BIN
-[ -f "$FAKE_BIN" ] || $NUGET_BIN Install FAKE -OutputDirectory $PACKAGE_DIR -ExcludeVersion
+[ -f "$FAKE_BIN" ] || mono $NUGET_BIN Install FAKE -OutputDirectory $PACKAGE_DIR -ExcludeVersion
 chmod +x $FAKE_BIN
 $FAKE_BIN build.fsx
+exitcode=$?
 git checkout $NUGET_BIN
+exit $exitcode
