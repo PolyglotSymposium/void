bac85bae1f17b40ea3754f4958aa4c09384bcbe4
Kazark <kazark@zoho.com>
2015-08-21 23:58:02 -0400

    Like I said, tons of tiny commits...

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 "src\.nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\packages\FAKE\tools\Fake.exe" build.fsx
+"tools\FAKE\tools\Fake.exe" build.fsx
