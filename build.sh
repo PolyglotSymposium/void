b90bb77c29887c91f4acff44f3ce747490af4b6d
Kazark <kazark@zoho.com>
2015-11-13 14:20:55 -0500

    FIXED - updated the build scripts to play with Paket
    Not sure if this works on Linux yet. Hopefully Travis CI will tell me
    that soon.

--- build.sh
+++ build.sh
@@ -2,15 +2,18 @@
 
 ROOT_DIR=$PWD
 SOURCE_DIR=$PWD/src
-NUGET_DIR=$SOURCE_DIR/.nuget
-PACKAGE_DIR=tools/
-NUGET_BIN=$NUGET_DIR/NuGet.exe
+PAKET_DIR=$SOURCE_DIR/.paket
+PACKAGE_DIR=packages/
+PAKET_BOOTSTRAPPER_BIN=$PAKET_DIR/paket.bootstrapper.exe
+PAKET_BIN=$PAKET_DIR/paket.exe
 FAKE_BIN=$PACKAGE_DIR/FAKE/tools/FAKE.exe
 
-chmod +x $NUGET_BIN
-[ -f "$FAKE_BIN" ] || mono $NUGET_BIN Install FAKE -OutputDirectory $PACKAGE_DIR -ExcludeVersion
+chmod +x $PAKET_BOOTSTRAPPER_BIN
+chmod +x $PAKET_BIN
+mono $PAKET_BOOTSTRAPPER_BIN
+mono $PAKET_BIN restore
 chmod +x $FAKE_BIN
 mono $FAKE_BIN build.fsx
 exitcode=$?
-git checkout $NUGET_BIN
+git checkout $PAKET_BOOTSTRAPPER_BIN
 exit $exitcode
