5d5191a7c2d48475ff449c8d19d4d4dd7dfd131c
Kazark <kazark@zoho.com>
2016-02-19 17:17:01 -0500

    Create installer for Void!
    FAKE allowed me to get up and running with WiX real quickly. That's a
    win!
    
    However if you run through it there's a bunch of Lorem Ipsum and
    whatnot...
    
    Also, it installs all the test code as well, which it should not.
    
    Also not sure it modifies the path at all.

--- build.fsx
+++ build.fsx
@@ -1,9 +1,11 @@
 #r "./packages/FAKE/tools/FakeLib.dll"
 open Fake
 open Fake.NuGet.Install
+open System.IO
 
 // Properties
 let buildDir = "./build/"
+let version = "0.0.2"
 
 // Targets
 Target "Clean" (fun _ ->
@@ -38,6 +40,75 @@ Target "JustRunTests" (fun _ ->
     trace "Test run completed!"
 )
 
+(* Got started with script from here:
+ * http://fsharp.github.io/FAKE/wix.html
+ * *)
+Target "BuildWixInstall" (fun _ ->
+    (* This defines which files should be collected when running
+     * bulkComponentCreation *)
+    let fileFilter (file : FileInfo) =
+        file.Extension = ".dll" || file.Extension = ".exe" || file.Extension = ".config"
+        
+    (* Collect Files which should be shipped. Pass directory with your
+     * deployment output for buildDir *)
+    let components = bulkComponentCreation fileFilter (DirectoryInfo buildDir)
+
+    (* Collect component references for usage in features *)
+    let componentRefs = components |> Seq.map (fun comp -> comp.ToComponentRef())
+
+    let completeFeature = generateFeatureElement (fun f -> 
+                                                    {f with  
+                                                        Id = "Complete"
+                                                        Title = "Complete Feature"
+                                                        Level = 1 
+                                                        Description = "Installs all features"
+                                                        Components = componentRefs
+                                                        Display = Expand 
+                                                    })
+
+    (* Generates a predefined WiX template with placeholders which will be
+     * replaced in "FillInWiXScript" *)
+    let wxsFile = "SetupTemplate.wxs"
+    generateWiXScript wxsFile
+
+    let WiXUIMondo = generateUIRef (fun f -> {f with Id = "WixUI_Mondo" })
+
+    let WiXUIError = generateUIRef (fun f -> {f with Id = "WixUI_ErrorProgressText" })
+
+    let MajorUpgrade = generateMajorUpgradeVersion(
+                            fun f ->
+                                {f with 
+                                    Schedule = MajorUpgradeSchedule.AfterInstallExecute
+                                    DowngradeErrorMessage = "A later version is already installed, exiting."
+                                })
+
+    FillInWiXTemplate "" (fun f ->
+                                {f with
+                                    ProductCode = System.Guid.NewGuid() // Guid which should be generated on every build
+                                    ProductName = "Void"
+                                    Description = "A fearless modern text editor in the spirit of Vim"
+                                    ProductLanguage = 1033
+                                    ProductVersion = version
+                                    ProductPublisher = "Polyglot Symposium"
+                                    UpgradeGuid = System.Guid.Parse "1b0f1b5e-90fd-4870-8a27-628f94f97d3b"
+                                    MajorUpgrade = [MajorUpgrade]
+                                    UIRefs = [WiXUIMondo; WiXUIError]
+                                    ProgramFilesFolder = ProgramFiles32
+                                    Components = components
+                                    BuildNumber = "0"
+                                    Features = [completeFeature]
+                                })
+        
+
+    (* Run the WiX tools *)
+    WiX (fun p -> {p with ToolDirectory = "packages/WiX/tools"}) (buildDir + "Void.msi") wxsFile
+)
+
+Target "RebuildMsi" (fun _ ->
+    trace "Build MSI completed!"
+)
+
+
 // Dependencies
 "Test"
   ==> "JustRunTests"
@@ -49,6 +120,12 @@ Target "JustRunTests" (fun _ ->
 
 "Test"
   ==> "Rebuild"
+
+"Rebuild"
+ ==> "RebuildMsi"
+
+"BuildWixInstall"
+ ==> "RebuildMsi"
 
 // start build
 RunTargetOrDefault "Rebuild"
